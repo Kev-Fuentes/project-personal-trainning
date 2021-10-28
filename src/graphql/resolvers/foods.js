41cb03627eb25771ec8e81c9bdadd8637da549f0
Kevin fuentes <kfuentes@condorlabs.io>
2021-10-28 15:00:29 -0500

    graphql mutations and querys finish

--- src/graphql/resolvers/foods.js
+++ src/graphql/resolvers/foods.js
@@ -4,15 +4,23 @@ const { foodsModel } = require('../../models');
 const resolvers = {
   Query: {
     foods: async () => {
+
       const foods = await foodsModel.find();
-      return foods;
+      return foods
+
     },
+    foodById: async (_, { id }) => {
+      const foods = await foodsModel.findById({ _id: id });
+      return foods
+    }
   },
   Mutation: {
-    createFood: async (_, { input }) => {
-      const newFood = await new foodsModel(input);
-      await newFood.save();
-      return newFood;
+    postFood: async (_, { input }) => {
+
+      const newfood = await new foodsModel(input);
+      newfood.save();
+      return newfood;
+
     },
     updateFood: (_, { _id, input }) => {
       return foodsModel.findByIdAndUpdate(_id, input, { new: true });
--- src/helpers/index.js
+++
@@ -1,6 +0,0 @@
-const findValidation = require('./findValidation');
-
-
-module.exports = {
-    findValidation
-}
