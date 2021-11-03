4019c29c792ee509b1d56b956a92ce87217e3ade
Kevin fuentes <kfuentes@condorlabs.io>
2021-11-03 12:38:09 -0500

    error test

--- src/controller/foodsController.js
+++ src/controller/foodsController.js
@@ -1,25 +1,15 @@
 'use strict';
 const { Food } = require('../models');
 const { schemaPostFood, schemaPatchFood } = require('../schemas');
-const { redis } = require('../config');
 const logger = require('@condor-labs/logger');
+
 const {
   status: { OK, BAD_RESQUEST, CREATE, ERROR_400, ERROR_404, EXISTING_RESOURCE, NOT_FOUND, SUCCESS },
 } = require('../constants');
 
 const getFoods = async (req, res) => {
-  const client = await redis();
-
   try {
-    const reply = await client.get('foods');
-    if (reply) {
-      logger.log('DATA FROM READIS');
-      return res.status(OK).json({ foods: JSON.params(reply), messages: SUCCESS });
-    }
-
     const foods = await Food.find();
-    await client.set('foods', JSON.stringify(foods));
-
     res.status(OK).json({ foods, messages: SUCCESS });
   } catch (error) {
     logger.err('Error get food', error);
