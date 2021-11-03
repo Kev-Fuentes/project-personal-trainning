dce7422e973ffdd2b5a117d70f02c01301eee609
Kevin fuentes <kfuentes@condorlabs.io>
2021-11-03 17:23:03 -0500

    redis renew implementation

--- tests/foods.spec.js
+++ tests/foods.spec.js
@@ -1,14 +1,8 @@
-const { initialFoods, newFood, api, mongoose, redis, getAllContentFromFoods } = require('./helpers');
-const { Food } = require('../src/models');
 
-beforeEach(async () => {
-  await Food.deleteMany({});
-  for (const food of initialFoods) {
-    const foodObj = new Food(food);
-    foodObj.save();
-  }
-});
-
+const app = require('../src/app');
+const supertest = require('supertest');
+const api = supertest(app);
+const { initialFoods } = require('./helpers')
 describe(' GET FOODS', () => {
   test('should repond with 200 status code', async () => {
     await api
@@ -21,76 +15,7 @@ describe(' GET FOODS', () => {
     const response = await api.get('/api/v1/foods');
     expect(response.body.foods).toHaveLength(initialFoods.length);
   });
+
 });
 
-describe('GET FOOD BY ID', () => {
-  test('should repond with a food by id', async () => {
-    const { response: responseAll } = await getAllContentFromFoods();
-    const {
-      body: { foods },
-    } = responseAll;
-    const [food] = foods;
-    await api
-      .get(`/api/v1/foods/${food._id}`)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-      .then((response) => {
-        expect(response.body.food.name).toBe(food.name);
-      });
-  });
-});
 
-describe('UPDATE FOOD', () => {
-  test('should repond with update a food', async () => {
-    const { response: responseAll } = await getAllContentFromFoods();
-    const {
-      body: { foods },
-    } = responseAll;
-    const [food] = foods;
-    const updateName = {
-      name: 'Panqueque',
-    };
-    await api
-      .patch(`/api/v1/foods/${food._id}`)
-      .send(updateName)
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-    const { names } = await getAllContentFromFoods();
-    expect(names).toContain(updateName.name);
-  });
-});
-
-describe(' POST FOOD', () => {
-  test('should repond with post a food', async () => {
-    await api
-      .post('/api/v1/foods')
-      .send(newFood)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
-    const { names, response } = await getAllContentFromFoods();
-
-    expect(response.body.foods).toHaveLength(initialFoods.length + 1);
-    expect(names).toContain(newFood.name);
-  });
-});
-
-describe(' DELETE FOOD', () => {
-  test('should repond with  delete food', async () => {
-    const { response: responseAll } = await getAllContentFromFoods();
-    const {
-      body: { foods },
-    } = responseAll;
-    const [foodToDelete] = foods;
-    await api.delete(`/api/v1/foods/${foodToDelete._id}`).expect(200);
-
-    const { names, response: responseOneDelete } = await getAllContentFromFoods();
-    expect(responseOneDelete.body.foods).toHaveLength(initialFoods.length - 1);
-    expect(names).not.toContain(foodToDelete.name);
-  });
-});
-
-afterAll(async () => {
-  const redisc = await redis.getClient();
-  await mongoose.disconnect();
-  await redisc.quit();
-});
