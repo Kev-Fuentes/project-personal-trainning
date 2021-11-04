76f59f3078c22fc2d723f6411ddebc43dc424f82
Kevin fuentes <kfuentes@condorlabs.io>
2021-11-04 16:56:12 -0500

    .

--- src/config/mongodb/setting.js
+++ src/config/mongodb/setting.js
@@ -10,13 +10,14 @@ const {
   MONGO_SSL,
   MONGO_USER,
   MONGO_PORT,
+  MONGO_DATABASE_TEST,
 } = process.env;
 
 const settingPro = {
   connectionName: MONGO_CONNECTION_NAME,
   host: MONGO_HOST,
   port: parseInt(MONGO_PORT),
-  database: MONGO_DATABASE,
+  database: process.env.NODE_ENV === 'test' ? MONGO_DATABASE_TEST : MONGO_DATABASE,
   user: MONGO_USER,
   password: MONGO_PASSWORD,
   ssl: MONGO_SSL === '1',
@@ -34,7 +35,7 @@ const settingDev = {
   ssl: MONGO_SSL === '1',
 };
 
-const setting = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ? settingPro : settingDev;
+const setting = process.env.NODE_ENV === 'production' ? settingPro : settingDev;
 
 const MongoDB = require('@condor-labs/mongodb')(setting);
 
